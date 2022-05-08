a777639ca690639ff3a176b3013a324ca98a8295
chesteric31 <chesteric31@gmail.com>
2022-05-08 22:14:31 +0200

    1 file modified

--- src/components/Heroes.vue
+++ src/components/Heroes.vue
@@ -16,6 +16,19 @@
 import HeroDetail from './HeroDetail.vue';
 import Hero from '../hero';
 
+const ALL = [
+  new Hero(11, 'Dr Nice'),
+  new Hero(12, 'Narco'),
+  new Hero(13, 'Bombasto'),
+  new Hero(14, 'Celeritas'),
+  new Hero(15, 'Magneta'),
+  new Hero(16, 'RubberMan'),
+  new Hero(17, 'Dynama'),
+  new Hero(18, 'Dr IQ'),
+  new Hero(19, 'Magma'),
+  new Hero(20, 'Tornado'),
+];
+
 export default {
   name: 'Heroes',
   components: {
@@ -24,18 +37,7 @@ export default {
   data() {
     return {
       selectedHero: Hero,
-      heroes: [
-        new Hero(11, 'Dr Nice'),
-        new Hero(12, 'Narco'),
-        new Hero(13, 'Bombasto'),
-        new Hero(14, 'Celeritas'),
-        new Hero(15, 'Magneta'),
-        new Hero(16, 'RubberMan'),
-        new Hero(17, 'Dynama'),
-        new Hero(18, 'Dr IQ'),
-        new Hero(19, 'Magma'),
-        new Hero(20, 'Tornado')
-      ],
+      heroes: ALL,
     };
   },
   computed: {
